17a1a9f32f7b3b5e834a07ef496d8c9208c1c935
Christopher Boyle <cboyle824@gmail.com>
2021-06-16 14:03:11 -0400

    Completed Tasks 1 & 2

--- index.js
+++ index.js
@@ -25,11 +25,9 @@ function Airplane(name) {
   // 👇 COMPLETE YOUR WORK BELOW 👇
   // 👇 COMPLETE YOUR WORK BELOW 👇
   */
-  function test() {
-    console.log('test for commit and push')
-  }
+  
 
-  test()
+
   /*
     TASK 1
       - Write a Person Constructor that initializes `name` and `age` from arguments.
@@ -43,14 +41,25 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name
+    this.age = age
+    this.stomach = []
   }
- 
- 
 
-  
-  
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length <= 10){
+      this.stomach.push(edible)
+    }
+  }
+
+  Person.prototype.poop = function(){
+    this.stomach = []
+  }
+
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`
+  }
   
   
   /*
@@ -67,10 +76,18 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model
+    this.milesPerGallon = milesPerGallon
+    this.tank = 0
+    this.odometer = 0
+  }
+
+  Car.prototype.fill = function(gallons){
+    this.tank += gallons
   }
   
+
   
   /*
     TASK 3
